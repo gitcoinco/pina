2e50dd656863b1a93296a89a5677d6a25d8303de
Andrea Franz <andrea@gravityblast.com>
2023-10-20 11:07:52 +0200

    add http router and port/public flags

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var (
+	publicPath string
+	port       int
+)
+
+func init() {
+	flag.IntVar(&port, "port", 0, "http server port")
+	flag.StringVar(&publicPath, "public", "", "public path")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
 
 func main() {
-	fmt.Printf("Hello World\n")
+	flag.Parse()
+	if port == 0 || publicPath == "" {
+		fmt.Println("port and public path flags are mandatory")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	router := httprouter.New()
+	router.GET("/", indexHandler)
+	router.NotFound = http.FileServer(http.Dir(publicPath))
+
+	binding := fmt.Sprintf(":%d", port)
+	fmt.Printf("listening: %s\n", binding)
+	fmt.Printf("public path: %s\n", publicPath)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
 }
