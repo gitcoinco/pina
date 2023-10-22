2b6406045a4a9a444c8da4f03c2ada05a2e2168e
Andrea Franz <andrea@gravityblast.com>
2023-10-22 22:30:45 +0200

    add pinJSONHandler

--- handlers_test.go
+++ handlers_test.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	assert "github.com/gravityblast/miniassert"
+	"github.com/julienschmidt/httprouter"
 )
 
+func newTestRouter(t *testing.T) (*httprouter.Router, string) {
+	publicPath := t.TempDir()
+	router, err := newRouter(publicPath)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	return router, filepath.Join(publicPath, "ipfs")
+}
+
 func TestIndexHandler(t *testing.T) {
-	router := newRouter("./")
-	handler, _, _ := router.Lookup("GET", "/")
+	router, _ := newTestRouter(t)
+
 	w := httptest.NewRecorder()
-	handler(w, nil, nil)
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	router.ServeHTTP(w, req)
 
 	resp := w.Result()
 	body, _ := io.ReadAll(resp.Body)
@@ -26,20 +44,13 @@ func TestIndexHandler(t *testing.T) {
 }
 
 func TestStaticFiles(t *testing.T) {
-	publicPath := t.TempDir()
-	ipfsPath := filepath.Join(publicPath, "ipfs")
-	err := os.Mkdir(ipfsPath, 0755)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	router, ipfsPath := newTestRouter(t)
 	filePath := filepath.Join(ipfsPath, "test.txt")
-	err = os.WriteFile(filePath, []byte("Static file example"), 0755)
+	err := os.WriteFile(filePath, []byte("Static file example"), 0755)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	router := newRouter(publicPath)
 	w := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodGet, "/ipfs/test.txt", nil)
 	router.ServeHTTP(w, req)
@@ -50,4 +61,41 @@ func TestStaticFiles(t *testing.T) {
 	assert.Equal(t, 200, resp.StatusCode)
 	assert.Equal(t, "text/plain; charset=utf-8", resp.Header.Get("Content-Type"))
 	assert.Equal(t, "Static file example", string(body))
+}
+
+func TestPinJSONHandler(t *testing.T) {
+	router, ipfsPath := newTestRouter(t)
+
+	w := httptest.NewRecorder()
+	reqBody := bytes.NewBufferString(`{"pinataContent": {"foo": {"bar": "baz"}}}`)
+	req := httptest.NewRequest(http.MethodPost, "/pinning/pinJSONToIPFS", reqBody)
+	router.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, "text/plain; charset=utf-8", resp.Header.Get("Content-Type"))
+
+	var responseBody PinJSONResponseBody
+	err := json.NewDecoder(resp.Body).Decode(&responseBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, responseBody.IpfsHash, "foo")
+	assert.Equal(t, responseBody.PinSize, 10)
+	assert.NotEqual(t, "", responseBody.Timestamp)
+
+	fileName := filepath.Join(ipfsPath, "test.json")
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, `{"foo":{"bar":"baz"}}`, strings.TrimSpace(string(content)))
 }
--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type PinJSONRequestBody struct {
+	PinataContent interface{} `json:"pinataContent"`
+}
+
+type PinJSONResponseBody struct {
+	IpfsHash  string `json:"ipfsHash"`
+	PinSize   int    `json:"pinSize"`
+	Timestamp string `json:"timestamp"`
+}
+
 var (
 	publicPath string
+	ipfsPath   string
 	port       int
 )
 
@@ -19,17 +33,62 @@ func init() {
 	flag.IntVar(&port, "port", 0, "http server port")
 	flag.StringVar(&publicPath, "public", "", "public path")
 }
+func handleError(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprint(w, "server error1")
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Hello World")
 }
 
-func newRouter(publicPath string) *httprouter.Router {
+func pinJSONHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var body PinJSONRequestBody
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+
+	fileName := "test.json"
+	f, err := os.Create(filepath.Join(ipfsPath, fileName))
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+	defer f.Close()
+
+	err = json.NewEncoder(f).Encode(body.PinataContent)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(&PinJSONResponseBody{
+		IpfsHash:  "foo",
+		PinSize:   10,
+		Timestamp: time.Now().UTC().Format(time.RFC3339),
+	})
+
+	if err != nil {
+		handleError(w, err)
+	}
+}
+
+func newRouter(publicPath string) (*httprouter.Router, error) {
+	ipfsPath = filepath.Join(publicPath, "ipfs")
+	err := os.MkdirAll(ipfsPath, 0755)
+	if err != nil {
+		return nil, err
+	}
+
 	router := httprouter.New()
 	router.GET("/", indexHandler)
+	router.POST("/pinning/pinJSONToIPFS", pinJSONHandler)
 	router.NotFound = http.FileServer(http.Dir(publicPath))
 
-	return router
+	return router, nil
 }
 
 func main() {
@@ -40,7 +99,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	router := newRouter(publicPath)
+	router, err := newRouter(publicPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 	binding := fmt.Sprintf(":%d", port)
 	fmt.Printf("listening: %s\n", binding)
 	fmt.Printf("public path: %s\n", publicPath)
